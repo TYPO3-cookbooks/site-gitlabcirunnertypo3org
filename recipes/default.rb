32dc37f9068637b3a84f4d8dc371ad18489c90b1
Steffen Gebert <steffen.gebert@typo3.org>
2017-10-04 07:43:31 +0200

    [BUGFIX] Handle registration correctly
    Using the registration token, the Gitlab server returns as an auto-generated
    token. This is written to config.toml and we must leave it there.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,28 +1,42 @@
+# needed for parsing the config file
+chef_gem 'toml'
+require 'toml'
+require 'uri'
+
 include_recipe 't3-base'
-
 include_recipe 'site-dockertypo3org'
-
 include_recipe 'gitlab-ci-runner'
-
 include_recipe 't3-chef-vault'
 
-name = 'docker'
-# token = chef_vault_password('git-t3o.typo3.org', node['fqdn'], 'token')
-token = chef_vault_password('git-t3o.typo3.org', 'test', 'token')
+gitlab_host = URI(node['site-gitlabcirunnertypo3org']['gitlab_url']).host
+registration_token = chef_vault_password(gitlab_host, 'runner', 'registration_token')
 
-
-template '/etc/gitlab-runner/config.toml' do
-  variables(
-    name: name,
-    url: node['site-gitlabcirunnertypo3org']['gitlab_url'],
-    token: token
-  )
-end
-
+# this registers the runner at the gitlab server using the registration_token and writes
+# some information to the config file, including an auto-generated token (NOT matching the
+# registration_token)
+name = "#{node['fqdn']}-docker"
 gitlab_ci_runner name do
   options(
-    registration_token: token,
+    registration_token: registration_token,
     url: node['site-gitlabcirunnertypo3org']['gitlab_url'],
-    executor: 'docker'
+    executor: 'docker',
+    :'docker-image' => 'docker:1',
+    :'tag-list' => ['docker']
   )
 end
+
+# replace config file with our parameters
+template '/etc/gitlab-runner/config.toml' do
+  variables lazy {
+    # we don't know the token, so read it from the file on disk before replacing it
+    config_on_disk = TOML.load_file('/etc/gitlab-runner/config.toml')
+    {
+      name: name,
+      url: node['site-gitlabcirunnertypo3org']['gitlab_url'],
+      token: config_on_disk['runners'].first['token']
+    }
+  }
+  notifies :restart, 'service[gitlab-runner]'
+end
+
+service 'gitlab-runner'

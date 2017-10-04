e3c3a9f14e72cab0cf1e047cbbb512490225d458
Steffen Gebert <steffen.gebert@typo3.org>
2017-10-04 20:51:46 +0200

    [TESTS] Correct usage of nginx default site

--- test/fixtures/cookbooks/gitlab-dummy-server/recipes/default.rb
+++ test/fixtures/cookbooks/gitlab-dummy-server/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'nginx'
 
-cookbook_file '/etc/nginx/sites-available/default' do
+cookbook_file '/etc/nginx/conf.d/default.conf' do
   source 'nginx-default.conf'
   notifies :restart, 'service[nginx]', :immediately
 end
